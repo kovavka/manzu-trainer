70fadb4cfc4bd406793272ce454f57cadda0292e
Gin Kapger <gikapger@gmail.com>
2026-04-22 22:23:01 +0200

    Remove sprite logic from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css'
 import {Main} from "./screens/Main.tsx";
 import {useEffect} from "react";
-import sprite from './assets/sprite.svg?raw'
 import {isMobile} from "./services/Ulils.ts";
-
 
 function App() {
   useEffect(() => {
-    let node = document.querySelector('.sprite')!
-    node.innerHTML = sprite
-    document.body.appendChild(node)
-
     if (isMobile()) {
       document.querySelector('#root')!.classList.add('mobile')
     }
